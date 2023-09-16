dfa89e1ffc0638dc414e2d5582dba5af1200034f
0Donato1 <denis08b@gmail.com>
2023-09-16 15:29:01 +0300

    #Lesson 2

---
+++ OOP_Class_Atr_Methods/main.py
@@ -0,0 +1,32 @@
+from student import Student
+
+budnychenko = Student("Denys", 15.7, "S2816", 1600.00)
+solop = Student("Illya", 16.1, "S2816", 105.00)
+oliynyk = Student("Kostya", 14.2, "S2816", 165.00)
+
+students = []
+
+students.append(budnychenko)
+students.append(solop)
+students.append(oliynyk)
+
+for student in students:
+    student.ShowInfo()
+print("----------------------------------------------------------------------")
+iphone15price = 1400
+budnychenko.Buy(iphone15price)
+solop.Buy(iphone15price)
+oliynyk.Buy(iphone15price)
+
+for student in students:
+    student.ShowInfo()
+
+days = 365
+for day in days:
+    if(day > 358 and day < 7):
+
+
+
+
+
+
---
+++ OOP_Class_Atr_Methods/student.py
@@ -0,0 +1,21 @@
+class Student:
+
+    def __init__(self, name:str, age:float, group:str, money:float):
+        self.Name = name
+        self.Age = age
+        self.Group = group
+        self.Money = money
+
+    def ShowInfo(self):
+        print(f"Name:\t{self.Name}\n"
+              f"Age: \t{self.Age}\n"
+              f"Group:\t{self.Group}\n"
+              f"Money:\t{self.Money}$")
+
+    def Buy(self, price:float):
+        if(self.Money >= price):
+            self.Money -= price
+            print("Nice bought!")
+        else:
+            print(f"Not enough money!\n"
+                  f"Your bugget:\t{self.Money}$")
