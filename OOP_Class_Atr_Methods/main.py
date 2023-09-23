ecd3d3ce7283fe2c954c2422c142fc3d90996fcb
0Donato1 <denis08b@gmail.com>
2023-09-23 12:42:06 +0300

    #Lesson 2

--- OOP_Class_Atr_Methods/main.py
+++ OOP_Class_Atr_Methods/main.py
@@ -1,23 +1,17 @@
-from student import Student
+from student import *
 
-budnychenko = Student("Denys", 15.7, "S2816", 1600.00)
-solop = Student("Illya", 16.1, "S2816", 105.00)
-oliynyk = Student("Kostya", 14.2, "S2816", 165.00)
+
+budnychenko = Student("Denys", 15.7, "S2816", 0)
 
 students = []
 
 students.append(budnychenko)
-students.append(solop)
-students.append(oliynyk)
 
 for student in students:
     student.ShowInfo()
-print("----------------------------------------------------------------------")
-iphone15price = 1400
-budnychenko.Buy(iphone15price)
-solop.Buy(iphone15price)
-oliynyk.Buy(iphone15price)
 
+
+budnychenko.Work()
 for student in students:
     student.ShowInfo()
 '''
--- OOP_Class_Atr_Methods/student.py
+++ OOP_Class_Atr_Methods/student.py
@@ -1,21 +1,114 @@
 class Student:
 
-    def __init__(self, name:str, age:float, group:str, money:float):
+    def __init__(self, name:str, age:float, group:str, earning:float):
         self.Name = name
         self.Age = age
         self.Group = group
-        self.Money = money
+        self.Earning = earning
 
     def ShowInfo(self):
         print(f"Name:\t{self.Name}\n"
               f"Age: \t{self.Age}\n"
               f"Group:\t{self.Group}\n"
-              f"Money:\t{self.Money}$")
+              f"Money:\t{self.Earning}$")
 
-    def Buy(self, price:float):
-        if(self.Money >= price):
-            self.Money -= price
-            print("Nice bought!")
-        else:
-            print(f"Not enough money!\n"
-                  f"Your bugget:\t{self.Money}$")
+
+    def Work(self):
+        month = 0
+        while month != 12:
+            day = 0
+            while day != 30:
+                day += 1
+            else:
+                self.Earning += 1000
+                day -= 30
+                month += 1
+                print("----------------------------------------------")
+                print(f"Now is {month} month\n"
+                      f"Your sallary for this month: \t1000$\n"
+                      f"Your bugget:\t{self.Earning}$")
+
+            if month == 1:
+                self.Earning -= 250
+                print(f"You pay for your apartments: \t250$\n"
+                      f"Now your bugget:\t{self.Earning}$")
+
+            if month == 2:
+                self.Earning += 500
+                print(f"You had a birthday, your friends presented to you: \t500$\n"
+                      f"Now your bugget:\t{self.Earning}$ ")
+                self.Earning -= 250
+                print(f"You pay for your apartments: \t250$\n"
+                      f"Now your bugget:\t{self.Earning}$")
+
+            if month == 3:
+                self.Earning -= 300
+                print(f"In this month was International Womens's Day, you bought gifts worth: \t300$\n"
+                      f"Now your bugget:\t{self.Earning}$ ")
+                self.Earning -= 250
+                print(f"You pay for your apartments: \t250$\n"
+                      f"Now your bugget:\t{self.Earning}$")
+
+            if month == 4:
+                self.Earning -= 250
+                print(f"You pay for your apartments: \t250$\n"
+                      f"Now your bugget:\t{self.Earning}$")
+
+            if month == 5:
+                self.Earning -= 250
+                print(f"You pay for your apartments: \t250$\n"
+                      f"Now your bugget:\t{self.Earning}$")
+
+            if month == 6:
+                self.Earning -= 500
+                print(f"Your mum and dad had a birthday: \t500$\n"
+                      f"Now your bugget:\t{self.Earning}$ ")
+
+            if month == 7:
+                self.Earning -= 250
+                print(f"You pay for your apartments: \t250$\n"
+                      f"Now your bugget:\t{self.Earning}$")
+
+            if month == 8:
+                self.Earning -= 1500
+                print(f"Your holidays cost you: \t1500$\n"
+                      f"Now your bugget:\t{self.Earning}$ ")
+
+            if month == 9:
+                self.Earning -= 250
+                print(f"You pay for your apartments: \t250$\n"
+                      f"Now your bugget:\t{self.Earning}$")
+
+            if month == 10:
+                self.Earning -= 250
+                print(f"You pay for your apartments: \t250$\n"
+                      f"Now your bugget:\t{self.Earning}$")
+
+            if month == 11:
+                self.Earning -= 250
+                print(f"You pay for your apartments: \t250$\n"
+                      f"Now your bugget:\t{self.Earning}$")
+
+            if month == 12:
+                self.Earning -= 1000
+                print(f"You bought presents for you family and friends it cost you: \t1000$\n"
+                      f"Now your bugget:\t{self.Earning}$ ")
+                self.Earning += 500
+                print(f"You are good worker and you get: \t500$\n"
+                      f"Now your bugget:\t{self.Earning}$ ")
+                iphone15price = 1400
+                iphone15 = 0
+                self.Earning -= iphone15price
+                print(f"You bought Iphone 15 Pro Max, it costs you: \t1400$\n"
+                      f"Now your bugget:\t{self.Earning}$")
+                print("----------------------------------------------")
+
+
+
+
+
+
+
+
+
+
